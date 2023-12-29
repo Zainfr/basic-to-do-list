57c2505808c803f1d0a5d07a8bb3bfa1b5aeb7cc
Zain Shaikh <zxyn59@gmail.com>
2023-12-29 23:37:39 +0530

    easypeasy

---
+++ script.js
@@ -0,0 +1,42 @@
+const toDoList = [
+  {
+    name: "",
+    dueDate: "",
+  },
+];
+
+function RenderToDo() {
+  let toDoListHtml = "";
+
+  for (let i = 0; i < toDoList.length; i++) {
+    const todoObejct = toDoList[i];
+
+    const name = todoObejct.name;
+    const dueDate = todoObejct.dueDate;
+    const html = `
+    <p>
+    ${name}${dueDate}
+    <button onclick="    
+    toDoList.splice(${i},1);
+    RenderToDo();">Delete</button>
+    </p>
+    `;
+    toDoListHtml += html;
+  }
+  document.querySelector(".js-todo-list").innerHTML = toDoListHtml;
+}
+
+function AddToDO() {
+  const inputElement = document.querySelector(".to-do-input");
+  const dueDateInputElement = document.querySelector(".js-due-date");
+  const name = inputElement.value;
+  const dueDate = dueDateInputElement.value;
+  toDoList.push({
+    name: name,
+    dueDate: dueDate,
+  });
+
+  console.log(toDoList);
+  inputElement.value = "";
+  RenderToDo();
+}
